e95189117b718f7d0e4c8d53780e43959e42a5cc
Juan Fuentes <juan@avocadosnetwork.com>
2021-09-10 00:45:13 -0500

    Third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import React, {useState}from 'react';
 import QrReader from 'react-qr-reader';
 import "../styles/App.css";
+
 const App = () => {
   //cambio de states a hook
   const [Qr, setQr] = useState({
-    result: 'Escanea tu QR'
+    result: 'Escanea tu QR',
+    camara: 'user'
   })
+  const [mirror, setMirror] = useState({mirrorVideo: false});
+
+  const [camara, setCamara] = useState('user')
  
+  const handleOnChange = e =>{
+    setQr({
+      ...Qr,
+      [e.target.name]: e.target.value,
+    })
+  }
+
   const handleScan = data => {
     if (data) {
       setQr({
+        ...Qr,
         result: data
       })
     }
@@ -17,17 +30,34 @@ const App = () => {
   const handleError = err => {
     console.error(err)
   }
+  const test = () =>{
+    console.log(Qr);
+  }
 
   return (
     <React.Fragment>
-    <h1>Tech demo</h1>
-    <QrReader
-      delay={300}
-      onError={handleError}
-      onScan={handleScan}
-      style={{ width: '30%' }}
-    />
-    <h2>{Qr.result}</h2>
+    <header>
+      <img width ="180px" height ="80px" src='http://baluher.com/wp-content/uploads/2018/04/baluher_logo_w.png' alt="I'm an easter egg" />
+      <h1>Tech demo</h1>
+    </header>
+    
+    <center>
+      <QrReader
+        delay={50}
+        onError={handleError}
+        onScan={handleScan}
+        onLoad={mirror.mirrorVideo}
+        facingMode={Qr.camara}
+        style={{ width: '30%' }}
+      />
+      <select name='camara' onChange={handleOnChange}>
+        <option value="user">User</option>
+        <option value="environment">environment</option>
+      </select>
+      <button onClick={test}>click me</button>
+    
+      <h2>{Qr.result}</h2>
+    </center>
     </React.Fragment>
   )
 }
